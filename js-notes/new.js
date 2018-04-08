8be14eb9b12ca108206629b232830f46a94179b5
muyi-macpro <17621660286@163.com>
2018-04-08 14:34:14 +0800

    绘制 函数创建的对象的原型链图

--- js-notes/new.js
+++ js-notes/new.js
@@ -2,6 +2,19 @@
 
 var arr = [1, 2, 3];
 
-console.log(1)
-
 console.log(Object.prototype)
+
+function Student(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Student.prototype.read = function () {
+    console.log(this.name + ' can read');
+}
+
+var xiaoming = new Student('xiaoming', 23);
+
+xiaoming.read();
+
+
